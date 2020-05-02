ed1af9a9e66559bebc1733c9a28c66b3bdf23695
Leon Si <leonzalion@gmail.com>
2020-05-02 17:16:59 -0400

    hi

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const http = require('http');
+const multer = require('multer');
+const upload = multer();
+const port = process.env.PORT || 4000;
 
 const app = express();
 
+app.set('view engine', 'html');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,7 +18,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', (req, res) => {
-  res.json({});
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.post('/upload', upload.single('soundBlob'), function(req, res, next) {
+  let uploadLocation = __dirname + '/public/uploads'  + req.file.originalname;
+  fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer)));
+  res.sendStatus(200);
 });
 
 // catch 404 and forward to error handler
@@ -22,16 +32,5 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 const server = http.createServer(app);
-server.listen(process.env.PORT || 4000);
+server.listen(port, () => console.log(`listening on port ${port}`));
