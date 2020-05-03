48167d04120c04046d2ab0096c15fcb4affcf281
Kevin Cui JY <kevincui1230@gmail.com>
2020-05-02 21:47:27 -0400

    Fix non-unique timestamps

--- receiver.js
+++ receiver.js
@@ -6,14 +6,6 @@ async function main() {
     const client = new speech.SpeechClient();
     const filename = 'assets/sample_audio.flac'
 
-    /**
-     * TODO(developer): Uncomment the following lines before running the sample.
-     */
-    // const gcsUri = 'gs://my-bucket/audio.raw';
-    // const encoding = 'Encoding of the audio file, e.g. LINEAR16';
-    // const sampleRateHertz = 16000;
-    // const languageCode = 'BCP-47 language code, e.g. en-US';
-
     const config = {
         enableWordTimeOffsets: true,
         encoding: 'FLAC',
@@ -50,16 +42,14 @@ async function main() {
             wordInfo.endTime.nanos / 100000000;
             console.log(`Word: ${wordInfo.word}`);
             console.log(`\t ${startSecs} secs - ${endSecs} secs`);
-            worddict[startSecs] = wordInfo.word;
+            if (startSecs in worddict) {
+                worddict[startSecs] += ' ' + wordInfo.word;
+            } else {
+                worddict[startSecs] = wordInfo.word;
+            }
         });
     });
     fs.writeFileSync('assets/sample.txt', transcription);
     fs.writeFileSync("assets/sample.json", JSON.stringify(worddict));
-    // const [response] = await client.recognize(request);
-    // const transcription = response.results
-    //     .map(result => result.alternatives[0].transcript)
-    //     .join('\n');
-    // console.log(`Transcription: ${transcription}`);
-    // fs.writeFileSync('assets/sample.txt', transcription);
 }
 main().catch(console.error);
