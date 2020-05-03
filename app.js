6df0e7d799228e104bfde1dfe08b8b03e220e23f
fishingguy456 <43834326+fishingguy456@users.noreply.github.com>
2020-05-03 01:33:41 -0400

    combined utils

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const multer = require('multer');
 const fs = require('fs');
 const upload = multer();
 const cors = require('cors');
+const utils = require('./utils.js');
 const port = process.env.PORT || 4000;
 const mongoose = require('mongoose');
 
@@ -33,6 +34,10 @@ app.post('/upload', upload.single('soundBlob'), function(req, res) {
   console.log(uploadLocation);
   fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer)));
   res.json({});
+  utils.converter(uploadLocation);
+  utils.receiver("./public/uploads/audio.flac");
+  utils.comparator("Sample question here"); //put the question here
+  utils.displayer();
 });
 
 // catch 404 and forward to error handler
