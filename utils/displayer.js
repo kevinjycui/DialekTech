ad404ba0009eb984eacc41c7c0dff420dcf47dd4
fishingguy456 <43834326+fishingguy456@users.noreply.github.com>
2020-05-03 02:24:10 -0400

    bracket

--- utils/displayer.js
+++ utils/displayer.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-const fs = require('fs');
-
-module.exports = (outputMessage) => {
-    document.getElementById("timestamp-message").innerHTML = outputMessage;
-};
-=======
 exports.default = () => {
     const fs = require("fs");
   
@@ -20,4 +13,3 @@ exports.default = () => {
     let main = document.getElementById("main");
     main.appendChild(div);
   };
->>>>>>> fec4fadbb017126d66f12c3c05aa4e79ff5ccdeb
