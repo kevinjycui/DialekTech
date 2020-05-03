3e065888499aae304c6497322321d9357727e7ab
fishingguy456 <43834326+fishingguy456@users.noreply.github.com>
2020-05-03 01:30:16 -0400

    merged util

---
+++ utils/utils.js
@@ -0,0 +1,42 @@
+exports.comparator = () =>{
+const fs = require('fs');
+let stringSimilarity = require('string-similarity');
+
+let question = 'literally put anything in here';
+
+question = question.trim();
+
+if (question.charAt(question.length-1)>=33||question.charAt(question.length-1)<=47) {
+    question = question.substring(0, question.length-1);
+}
+
+let len = question.split(" ").length;
+let timeJSON = JSON.parse(fs.readFileSync('./public/responses/response.json', 'utf8'));
+const timestamps = Object.keys(timeJSON);
+const words = Object.values(timeJSON);
+let checkWord = [];
+
+if(len<=words.length){
+    for(let i = 0; i < words.length-len; i++){
+        let ans = '';
+        for(let j = 0; j < len-1; j++){
+            ans+=words[i+j]+" ";
+        }
+        ans+=words[i+len-1];
+        checkWord.push(ans);
+    }
+    console.log(checkWord);
+
+    let matches = stringSimilarity.findBestMatch(question, checkWord);
+
+    for (let i=matches.bestMatchIndex; i<matches.bestMatchIndex+len; i++) {
+        console.log(words[i]);
+    }
+
+    console.log(`${timestamps[matches.bestMatchIndex]} to ${timestamps[matches.bestMatchIndex+len-1]}`);
+}
+
+else {
+    console.log('Your question is too long!');
+}
+}
